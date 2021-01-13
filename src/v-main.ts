bc512571a231cf1e389eaad0ad2c5782d98e6f84
Jeremy Chone <jeremy.chone@gmail.com>
2021-01-13 10:14:49 -0800

    . initial

---
+++ src/v-main.ts
@@ -0,0 +1,21 @@
+import { BaseHTMLElement, customElement, first, html, onEvent } from 'dom-native';
+
+@customElement('v-main') // same as customElements.define('v-main', IcoElement) 
+class MainView extends BaseHTMLElement { // extends native HTMLElement
+
+	#clickCount = 0; // private, transpiled by TypeScript.
+
+	@onEvent('pointerup', '.hello-box')
+	onHelloClick(evt: PointerEvent) {
+		first(this, '.hello-box strong')!.textContent = `CLICKED ${++this.#clickCount}`;
+	}
+
+	init() { // called once on the first connectedCallback
+
+		this.append(html`
+			<c-pie></c-pie>
+		`);
+
+	}
+
+}
